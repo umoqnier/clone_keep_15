3d91b02aabaa53fdb422b511fc94be7e6873497f
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2017-06-30 16:50:26 -0500

    Funcionando modelos personales

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Remainder, Prioridad
 
-# Register your models here.
+admin.site.register(Remainder)
+admin.site.register(Prioridad)
--- home/models.py
+++ home/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+
+class Prioridad(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10)
+    fecha_creacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name.upper()
+
+
 class Remainder(models.Model):
 
     PRIORIDADES = (
@@ -8,11 +18,27 @@ class Remainder(models.Model):
         ("BJ", "Baja"),
     )
 
+    MEDIA_CHOICES = (
+        ('Audio', (
+            ('vinyl', 'Vinyl'),
+            ('cd', 'CD'),
+        )
+         ),
+        ('Video', (
+            ('vhs', 'VHS Tape'),
+            ('dvd', 'DVD'),
+        )
+         ),
+        ('unknown', 'Unknown'),
+    )
+
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=60)
     descripcion = models.TextField()
     prioridad = models.CharField(max_length=7, choices=PRIORIDADES)
-    fecha_creacion = models.DateTimeField(auto_now=True)
+    prioridad_personal = models.ForeignKey(Prioridad, on_delete=models.CASCADE, default=1)
+    fecha_creacion_g = models.DateTimeField(auto_now=True)
 
-
+    def __str__(self):
+        return "Remainder N° " + str(self.id) + ": " + self.titulo
 
